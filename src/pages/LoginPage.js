7715fca732e0a8ccc6b0f20ea1ad81c951fe46e0
2201400 <127301228+2201400@users.noreply.github.com>
2023-06-20 15:05:44 +0900

    Update LoginPage.js
    ㅇ

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,5 +9,15 @@ class LoginPage extends HTMLDivElement {
 }
 
 window.customElements.define('login-page', LoginPage, { extends: 'div' });
+let imageArray = ["1.png", "2.png", "3.png", "4.png"];
+let imageIndex=0;
 
+function changeImage(){
+    img1.setAttribute("src", imageArray[imageIndex]);
+    imageIndex++;
+    if(imageIndex>=imageArray.length){
+    imageIndex=0;
+    }
+}
+setInterval(changeImage,2000);
 export default LoginPage;
